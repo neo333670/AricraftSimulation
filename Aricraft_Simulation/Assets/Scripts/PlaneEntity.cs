f130995e1d6767358b7ad372c08f8f08b47de2c7
neo333670 <neo333670@gmail.com>
2021-04-08 01:24:11 +0800

    checkpoint2

--- Aricraft_Simulation/Assets/Scripts/PlaneEntity.cs
+++ Aricraft_Simulation/Assets/Scripts/PlaneEntity.cs
@@ -7,18 +7,20 @@ public class PlaneEntity : MonoBehaviour
     float m_Velocity = 0;
     const float PLANE_ACCELERATION = 10f;
     const float PLANE_DECELERATION = 10f;
-    const float PLANE_MIN_VELOCITY = 10f;
-    const float PLANE_MAX_VELOCITY = 10f;
+    //const float PLANE_MIN_VELOCITY = 10f;
+    const float PLANE_MAX_VELOCITY = 20f;
 
     float m_RollVelocity = 0;
     const float ROLL_ACCELERATION = 100f;
     const float ROLL_DECELERATION = 100f;
     const float MAX_ROLL_VELOCITY = 100f;
 
-    float m_PitchVelocity = 0;
-    const float Pitch_ACCELERATION = 100f;
-    const float Pitch_DECELERATION = 100f;
-    const float MAX_Pitch_VELOCITY = 100f;
+    public float m_PitchVelocity = 0;
+    const float Pitch_ACCELERATION = 20f;
+    const float Pitch_DECELERATION = 50f;
+    const float MAX_Pitch_VELOCITY = 20f;
+
+    [SerializeField] Transform m_PlantBodyTrans;
 
     private void FixedUpdate(){
         ForwardControl();
@@ -27,6 +29,9 @@ public class PlaneEntity : MonoBehaviour
         this.transform.Translate(m_Velocity * Time.fixedDeltaTime * Vector3.right);
         this.transform.Rotate(m_RollVelocity *Time.fixedDeltaTime, 0, 0);
         this.transform.Rotate(0, 0, m_PitchVelocity * Time.fixedDeltaTime);
+        //visual effects
+        m_PlantBodyTrans.localEulerAngles = new Vector3(m_RollVelocity / MAX_ROLL_VELOCITY *1000f *Time.fixedDeltaTime,
+        0f, m_PitchVelocity/MAX_Pitch_VELOCITY *300f *Time.fixedDeltaTime);
     }
 
     void RollControl() {
@@ -40,8 +45,8 @@ public class PlaneEntity : MonoBehaviour
         else
         {
             m_RollVelocity = m_RollVelocity > 0 ?
-                Mathf.Min(0, m_RollVelocity - ROLL_DECELERATION * Time.fixedDeltaTime) :
-                Mathf.Max(0, m_RollVelocity + ROLL_DECELERATION * Time.fixedDeltaTime);
+                Mathf.Max(0, m_RollVelocity - ROLL_DECELERATION * Time.fixedDeltaTime) :
+                Mathf.Min(0, m_RollVelocity + ROLL_DECELERATION * Time.fixedDeltaTime);
         }
     }
 
@@ -60,8 +65,8 @@ public class PlaneEntity : MonoBehaviour
             m_PitchVelocity = Mathf.Max(-MAX_Pitch_VELOCITY, m_PitchVelocity - Pitch_ACCELERATION * Time.fixedDeltaTime);
         } else {
             m_PitchVelocity = m_PitchVelocity > 0 ?
-                Mathf.Min(0, m_PitchVelocity - Pitch_DECELERATION * Time.fixedDeltaTime) :
-                Mathf.Max(0, m_PitchVelocity + Pitch_DECELERATION * Time.fixedDeltaTime);
+                Mathf.Max(0, m_PitchVelocity - Pitch_DECELERATION * Time.fixedDeltaTime) :
+                Mathf.Min(0, m_PitchVelocity + Pitch_DECELERATION * Time.fixedDeltaTime);
         }
     }
 }
---
+++ Aricraft_Simulation/Assets/Scripts/TurretEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEntity : MonoBehaviour
+{
+    [SerializeField] GameObject m_target;
+    public GameObject m_Base;
+    public GameObject m_Gun;
+
+    private void FixedUpdate()
+    {   //base rotation
+        Vector3 diffVec = m_target.transform.position - this.transform.position;
+
+        var targetQuaternion = Quaternion.FromToRotation(
+            Vector3.left, new Vector3(diffVec.x, 0f, diffVec.z));
+        m_Base.transform.localRotation = targetQuaternion;
+        //gun rotation
+        Vector2 xzProjection = new Vector2(diffVec.x, diffVec.z);
+        float xzPjLength = xzProjection.magnitude;
+
+        var targetGunQuaternion = Quaternion.FromToRotation
+        (new Vector3(-xzPjLength, 0, 0), new Vector3(-xzPjLength, diffVec.y, 0));
+        m_Gun.transform.localRotation = targetGunQuaternion;
+    }
+}
